12681b26470878e764fb94372a4c471e187a6786
Shazil Jamal <shaziljamal7003@gmail.com>
2022-06-06 16:58:42 -0700

    Display List Commit

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+//In the same file (HeroesComponent class), define a component property called heroes to expose the HEROES array for binding.
+
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  //Add the following onSelect() method, which assigns the clicked hero from the template to the component's selectedHero.
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
 }
+}
+
